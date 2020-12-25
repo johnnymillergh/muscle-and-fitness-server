97aadcef7e3febbdd1a05403d7f72dc2c23d4c43
Johnny Miller (锺俊) <zhongjun@toguide.cn>
2020-12-25 14:28:34 +0800

    feat($AuthCenter): get services info

--- auth-center/src/main/java/com/jmsoftware/maf/authcenter/permission/controller/PermissionController.java
+++ auth-center/src/main/java/com/jmsoftware/maf/authcenter/permission/controller/PermissionController.java
@@ -1,9 +1,18 @@
 package com.jmsoftware.maf.authcenter.permission.controller;
 
 import com.jmsoftware.maf.authcenter.permission.service.PermissionService;
+import com.jmsoftware.maf.common.bean.ResponseBodyBean;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * <h1>PermissionController</h1>
@@ -13,8 +22,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Johnny Miller (锺俊), e-mail: johnnysviva@outlook.com
  * @date 5/11/20 8:34 AM
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@Api(tags = {"Permission API"})
 public class PermissionController {
     private final PermissionService permissionService;
+    private final DiscoveryClient discoveryClient;
+
+    @GetMapping("/permissions/services-info")
+    @ApiOperation(value = "Get services info", notes = "Get services info")
+    public ResponseBodyBean<?> servicesInfo() {
+        List<String> services = discoveryClient.getServices();
+        log.info("Service ID: {}", services);
+        val resultMap = new LinkedHashMap<String, Object>();
+        services.forEach(service -> {
+            val instances = discoveryClient.getInstances(service);
+            log.info("Instances: {}", instances);
+            resultMap.put(service, instances);
+        });
+        return ResponseBodyBean.ofSuccess(resultMap);
+    }
 }
