ac7aabcc3a83e471377ab1ccf269ddf808b581c8
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-02-08 16:16:30 +0800

    perf($api-gateway): add IP key resolver for request rate limiter

--- api-gateway/src/main/java/com/jmsoftware/maf/apigateway/universal/configuration/DiscoveryRouteConfiguration.java
+++ api-gateway/src/main/java/com/jmsoftware/maf/apigateway/universal/configuration/DiscoveryRouteConfiguration.java
@@ -1,12 +1,16 @@
 package com.jmsoftware.maf.apigateway.universal.configuration;
 
+import cn.hutool.core.util.ObjectUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.filter.FilterDefinition;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
 
@@ -21,16 +25,36 @@ import javax.annotation.PostConstruct;
 public class DiscoveryRouteConfiguration {
     private final DiscoveryLocatorProperties discoveryLocatorProperties;
 
+    /**
+     * Post construct.
+     *
+     * @see
+     * <a href='https://docs.spring.io/spring-cloud-gateway/docs/3.0.1/reference/html/#the-redis-ratelimiter'>The Redis RateLimiter</a>
+     */
     @PostConstruct
     void postConstruct() {
         val filter = new FilterDefinition();
         filter.setName("RequestRateLimiter");
         // TODO: arguments should be passed from configuration dynamically
+        // setting replenishRate=1, requestedTokens=1 and burstCapacity=1
+        // will result in a limit of 1 request per 1 second.
+        // setting replenishRate=1, requestedTokens=60 and burstCapacity=60 will result in a limit of 1 request/min.
         filter.addArg("redis-rate-limiter.replenishRate", "1");
         filter.addArg("redis-rate-limiter.burstCapacity", "1");
         filter.addArg("redis-rate-limiter.requestedTokens", "1");
         discoveryLocatorProperties.getFilters().add(filter);
         log.info("Added filter[{}] for discovery services, filters: {}", RedisRateLimiter.class.getSimpleName(),
                  discoveryLocatorProperties.getFilters());
+    }
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> {
+            val remoteAddress = exchange.getRequest().getRemoteAddress();
+            if (ObjectUtil.isNotNull(remoteAddress)) {
+                return Mono.just(remoteAddress.getHostName());
+            }
+            return Mono.just("unknown-address");
+        };
     }
 }
