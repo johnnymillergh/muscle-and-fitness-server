8e76578cdc1686e4cb0082c8241f12d8197c2975
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-02-18 14:54:44 +0800

    chore($Consul): add local Consul configuration
    [skip ci]

---
+++ docker/config/local-consul/startup.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+@REM ############################## auto-run ############################
+@REM # Author: Johnny Miller, date: 2021-02-08 09:23:29.314             #
+@REM # Copyright (c) Johnny Miller                                      #
+@REM # Capability: Windows 10 Pro (20H2)                                #
+@REM # Purpose:                                                         #
+@REM #   1. Integrate easy and simple deployment,                       #
+@REM ####################################################################
+
+SETLOCAL EnableDelayedExpansion
+@REM UTF-8
+chcp 65001
+
+cls
+TITLE local-consul
+CALL .\consul.exe agent -config-file="config.json"
