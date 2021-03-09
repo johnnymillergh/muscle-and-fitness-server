1b9bb6ded2005837fe8727eababb36be6ab5175f
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-03-09 15:50:17 +0800

    perf($RabbitMQ): make JSON message converter as default
    https://docs.spring.io/spring-amqp/reference/html/#async-annotation-conversion
    
    [skip ci]

--- spring-cloud-starter/src/main/java/com/jmsoftware/maf/springcloudstarter/configuration/RabbitmqConfiguration.java
+++ spring-cloud-starter/src/main/java/com/jmsoftware/maf/springcloudstarter/configuration/RabbitmqConfiguration.java
@@ -3,6 +3,10 @@ package com.jmsoftware.maf.springcloudstarter.configuration;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,5 +29,10 @@ public class RabbitmqConfiguration {
         val exchange = new TopicExchange(topicExchangeName);
         log.info("Created topic exchange: {}", exchange);
         return exchange;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
