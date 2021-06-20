605cc2f01d4221d93d2df6411950c5bff7733316
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-06-20 13:55:51 +0800

    build($static-resource-center): init sub module
    [skip ci]

---
+++ static-resource-center/src/main/java/com/jmsoftware/maf/authcenter/StaticResourceCenterApplication.java
@@ -0,0 +1,35 @@
+package com.jmsoftware.maf.authcenter;
+
+import com.jmsoftware.maf.springcloudstarter.helper.SpringBootStartupHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.util.StopWatch;
+
+/**
+ * <h1>StaticResourceCenterApplication</h1>
+ * <p>
+ * Change description here.
+ *
+ * @author Johnny Miller (锺俊), email: johnnysviva@outlook.com, date: 6/20/21 1:32 PM
+ **/
+@Slf4j
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class StaticResourceCenterApplication {
+    private static final StopWatch STOP_WATCH = new StopWatch();
+    private static SpringBootStartupHelper springBootStartupHelper;
+
+    public StaticResourceCenterApplication(SpringBootStartupHelper springBootStartupHelper) {
+        StaticResourceCenterApplication.springBootStartupHelper = springBootStartupHelper;
+    }
+
+    public static void main(String[] args) {
+        STOP_WATCH.start();
+        SpringApplication.run(StaticResourceCenterApplication.class, args);
+        springBootStartupHelper.stop(STOP_WATCH);
+    }
+}
