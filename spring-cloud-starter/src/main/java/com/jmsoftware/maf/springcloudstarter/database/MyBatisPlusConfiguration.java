3647951d4019220e181b4281d86e4af2907b961f
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-06-27 17:52:05 +0800

    fix($MyBatis): correct interceptors order

--- spring-cloud-starter/src/main/java/com/jmsoftware/maf/springcloudstarter/database/MyBatisPlusConfiguration.java
+++ spring-cloud-starter/src/main/java/com/jmsoftware/maf/springcloudstarter/database/MyBatisPlusConfiguration.java
@@ -2,14 +2,17 @@ package com.jmsoftware.maf.springcloudstarter.database;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -39,8 +42,28 @@ public class MyBatisPlusConfiguration {
         return new BlockAttackInnerInterceptor();
     }
 
+    /**
+     * Mybatis plus interceptor mybatis plus interceptor.
+     *
+     * @param paginationInnerInterceptor  the pagination inner interceptor
+     * @param blockAttackInnerInterceptor the block attack inner interceptor
+     * @return the mybatis plus interceptor
+     * @see <a href='https://baomidou.com/guide/interceptor.html'>MybatisPlusInterceptor</a>
+     */
     @Bean
-    public DynamicDataSourceInterceptor dynamicDataSourceInterceptor() {
+    @Order(1)
+    public Interceptor mybatisPlusInterceptor(PaginationInnerInterceptor paginationInnerInterceptor,
+                                              BlockAttackInnerInterceptor blockAttackInnerInterceptor) {
+        log.warn("Initial bean array: '{}'", MybatisPlusInterceptor.class.getSimpleName());
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+        mybatisPlusInterceptor.addInnerInterceptor(blockAttackInnerInterceptor);
+        return mybatisPlusInterceptor;
+    }
+
+    @Bean
+    @Order(2)
+    public Interceptor dynamicDataSourceInterceptor() {
         log.warn("Initial bean: '{}'", DynamicDataSourceInterceptor.class.getSimpleName());
         return new DynamicDataSourceInterceptor();
     }
