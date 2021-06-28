e9d935b183c05287bbc1c1a78f13a1e6140bbc02
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-06-29 07:48:02 +0800

    perf($auth-center): cache role info by user id

--- auth-center/src/main/java/com/jmsoftware/maf/authcenter/role/service/impl/RoleServiceImpl.java
+++ auth-center/src/main/java/com/jmsoftware/maf/authcenter/role/service/impl/RoleServiceImpl.java
@@ -2,23 +2,29 @@ package com.jmsoftware.maf.authcenter.role.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jmsoftware.maf.authcenter.role.entity.constant.RoleRedisKey;
 import com.jmsoftware.maf.authcenter.role.entity.persistence.Role;
 import com.jmsoftware.maf.authcenter.role.mapper.RoleMapper;
 import com.jmsoftware.maf.authcenter.role.service.RoleService;
 import com.jmsoftware.maf.common.domain.authcenter.role.GetRoleListByUserIdResponse;
 import com.jmsoftware.maf.springcloudstarter.configuration.MafConfiguration;
+import com.jmsoftware.maf.springcloudstarter.configuration.MafProjectProperty;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -32,10 +38,18 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements RoleService {
+    private final MafProjectProperty mafProjectProperty;
     private final MafConfiguration mafConfiguration;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Override
     public GetRoleListByUserIdResponse getRoleList(@NotNull Long userId) {
+        val key = String.format(mafProjectProperty.getProjectParentArtifactId()
+                                        + RoleRedisKey.GET_ROLE_LIST_BY_USER_ID.getKeyInfixFormat(), userId);
+        val hasKey = redisTemplate.hasKey(key);
+        if (BooleanUtil.isTrue(hasKey)) {
+            return JSONUtil.toBean(redisTemplate.opsForValue().get(key), GetRoleListByUserIdResponse.class);
+        }
         val roleList = this.getRoleListByUserId(userId);
         GetRoleListByUserIdResponse response = new GetRoleListByUserIdResponse();
         roleList.forEach(rolePersistence -> {
@@ -43,6 +57,7 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements Ro
             BeanUtil.copyProperties(rolePersistence, role);
             response.getRoleList().add(role);
         });
+        redisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(response), RandomUtil.randomLong(1, 7), TimeUnit.DAYS);
         return response;
     }
 
@@ -53,11 +68,11 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements Ro
 
     @Override
     public boolean checkAdmin(@NotEmpty List<@NotNull Long> roleIdList) {
-        final LambdaQueryWrapper<Role> wrapper = Wrappers.lambdaQuery();
+        val wrapper = Wrappers.lambdaQuery(Role.class);
         wrapper.select(Role::getName)
                 .in(Role::getId, roleIdList);
-        val rolePersistenceList = this.list(wrapper);
-        val roleNameSet = rolePersistenceList
+        val roleList = this.list(wrapper);
+        val roleNameSet = roleList
                 .stream()
                 .map(Role::getName)
                 .filter(roleName -> StrUtil.equals(mafConfiguration.getSuperUserRole(), roleName))
--- auth-center/src/main/java/com/jmsoftware/maf/authcenter/user/entity/constant/UserRedisKey.java
+++ auth-center/src/main/java/com/jmsoftware/maf/authcenter/user/entity/constant/UserRedisKey.java
@@ -1,6 +1,7 @@
 package com.jmsoftware.maf.authcenter.user.entity.constant;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Description: UserRedisKey, change description here.
@@ -8,14 +9,12 @@ import lombok.Getter;
  * @author Johnny Miller (锺俊), email: johnnysviva@outlook.com, date: 6/28/2021 3:18 PM
  **/
 @Getter
+@RequiredArgsConstructor
 public enum UserRedisKey {
     /**
      * Get user by login token key pattern, expired in random [1, 7) days
      */
     GET_USER_BY_LOGIN_TOKEN(":user:get_user_by_login_token:%s");
-    private final String keyInfixFormat;
 
-    UserRedisKey(String keyInfixFormat) {
-        this.keyInfixFormat = keyInfixFormat;
-    }
+    private final String keyInfixFormat;
 }
