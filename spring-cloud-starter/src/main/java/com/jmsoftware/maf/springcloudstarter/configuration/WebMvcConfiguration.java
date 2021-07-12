cbb9c7c56c85f7e1c10a7e6c282502ab65b4c3fa
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-07-12 14:21:20 +0800

    perf($Spring): refine CORS configuration

--- spring-cloud-starter/src/main/java/com/jmsoftware/maf/springcloudstarter/configuration/WebMvcConfiguration.java
+++ spring-cloud-starter/src/main/java/com/jmsoftware/maf/springcloudstarter/configuration/WebMvcConfiguration.java
@@ -1,8 +1,11 @@
 package com.jmsoftware.maf.springcloudstarter.configuration;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
 
 /**
  * <h1>WebMvcConfiguration</h1>
@@ -12,10 +15,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author Johnny Miller (锺俊), email: johnnysviva@outlook.com
  * @date 1/23/20 9:02 AM
  **/
+@Slf4j
 @RequiredArgsConstructor
 public class WebMvcConfiguration implements WebMvcConfigurer {
     /**
-     * Max age: 3600 seconds (1 hour)
+     * Max age: 3600 seconds (1 hour). Configure how long in seconds the response from a pre-flight request
+     * can be cached by clients. By default this is set to 1800 seconds (30 minutes).
      */
     private static final long MAX_AGE_SECS = 3600;
 
@@ -25,11 +30,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
      * @param registry CORS registry
      */
     @Override
-    @SuppressWarnings("BroadCORSAllowOrigin")
     public void addCorsMappings(CorsRegistry registry) {
+        log.info("Configuring CORS allowedOrigins: {}, allowedMethods: {}, allowedHeaders: {}", ALL, ALL, ALL);
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedOrigins(ALL)
+                .allowedMethods(ALL)
+                .allowedHeaders(ALL)
                 .maxAge(MAX_AGE_SECS);
     }
 }
