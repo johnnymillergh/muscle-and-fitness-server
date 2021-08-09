e36092b9808d47d223aebd42d23a8b2bfca6f2e7
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2021-08-09 15:10:14 +0800

    perf($POM): refine dependency scope

--- spring-cloud-starter/src/main/java/com/jmsoftware/maf/springcloudstarter/websocket/WebSocketConfiguration.java
+++ spring-cloud-starter/src/main/java/com/jmsoftware/maf/springcloudstarter/websocket/WebSocketConfiguration.java
@@ -4,17 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-import static org.springframework.web.cors.CorsConfiguration.ALL;
 
 /**
  * Description: WebSocketConfiguration, change description here.
@@ -29,10 +24,9 @@ import static org.springframework.web.cors.CorsConfiguration.ALL;
 @Slf4j
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
+@Import({WebSocketMessageBrokerConfiguration.class})
 @ConditionalOnClass({ServerEndpointExporter.class, RabbitTemplate.class})
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
-    private final RabbitProperties rabbitProperties;
-
+public class WebSocketConfiguration {
     @Bean
     public ChatRoomController greetingController(ChatRoomService chatRoomService) {
         log.warn("Initial bean: '{}'", ChatRoomController.class.getSimpleName());
@@ -44,39 +38,5 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
                                            ObjectMapper objectMapper) {
         log.warn("Initial bean: '{}'", ChatRoomService.class.getSimpleName());
         return new ChatRoomServiceImpl(simpMessagingTemplate, objectMapper);
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/web-socket", "/websocket", "/ws")
-                .setAllowedOriginPatterns(ALL)
-                .withSockJS();
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * <p>There are two destination prefixes defined: topic and queue. They follow the convention that destinations
-     * for messages to be carried on to all subscribed clients via the pub-sub model should be prefixed with &quot;
-     * topic&quot;. On the other hand, destinations for private messages are typically prefixed by &quot;queue&quot;
-     * .</p>
-     * <pre><code class='language-java' lang='java'>registry.enableSimpleBroker(&quot;/topic/&quot;, &quot;/queue/&quot;);
-     * </code></pre>
-     * <p>Defines the prefix app that is used to filter destinations handled by methods annotated with
-     * <code>@MessageMapping</code> which you will implement in a controller. The controller, after processing the
-     * message, will send it to the broker.</p>
-     */
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Use RabbitMQ as broker instead of in-memory message broker, enabling a full featured broker like RabbitMQ
-        registry.enableStompBrokerRelay("/topic/", "/queue/")
-                // We need to specify the host but not the port, cuz we're using the default STOMP relay port, which
-                // is "61613"
-                .setRelayHost(this.rabbitProperties.getHost())
-                .setClientLogin(this.rabbitProperties.getUsername())
-                .setClientPasscode(this.rabbitProperties.getPassword())
-                .setSystemLogin(this.rabbitProperties.getUsername())
-                .setSystemPasscode(this.rabbitProperties.getPassword());
-        registry.setApplicationDestinationPrefixes("/app");
     }
 }
