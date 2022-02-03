f6ee0890886f6f98c2799788b44c4dc536a5ea86
Johnny Miller (ZA) <johnnysviva@outlook.com>
2022-02-03 15:21:47 +0800

    build($BatchScript): update batch script to support DDD architecture
    [skip ci]

--- auth-center/auto-run-windows.cmd
+++ auth-center/auto-run-windows.cmd
@@ -1,63 +1,63 @@
 @ECHO OFF
-@REM ############################## auto-run ############################
-@REM # Author: 钟俊, date: 12:43:04 PM, Dec. 8, 2020                     #
-@REM # Copyright (c) 钟俊                                                #
-@REM # Capability: Windows 10 Pro (20H2)                                #
-@REM # Purpose:                                                         #
-@REM #   1. Integrate easy and simple deployment,                       #
-@REM #   2. Reduce memory usage of IntelliJ IDEA to run services,       #
-@REM #   3. Improve the efficiency of local development.                #
-@REM ####################################################################
+REM ############################## auto-run ############################
+REM # Author: Johnny Miller (锺俊), date: 15:11:04 PM, Feb. 3, 2022     #
+REM # Copyright (c) Johnny Miller (锺俊)                                #
+REM # Capability: Windows 11 Pro (21H2)                                #
+REM # Purpose:                                                         #
+REM #   1. Integrate easy and simple deployment,                       #
+REM #   2. Reduce memory usage of IntelliJ IDEA to run services,       #
+REM #   3. Improve the efficiency of local development.                #
+REM ####################################################################
 
 SETLOCAL EnableDelayedExpansion
-@REM UTF-8
+REM UTF-8
 chcp 65001
 
-@REM #################### Configurable Environment Variables ###################
+REM #################### Configurable Environment Variables ###################
 SET skipGitPull=true
 SET skipMavenBuild=true
 SET minimalJavaMajorVersion=11
-SET javaExe=C:\Users\Johnny\.sdkman\candidates\java\11.0.10.hs-adpt\bin\java.exe
-SET mavenActiveProfile="development-local"
+SET javaExe=C:\Users\za-zhongjun\.sdkman\candidates\java\11.0.12-open\bin\java.exe
+SET springActiveProfile="development-local"
 SET javaParameter=-Xms256m -Xmx256m -Dfile.encoding=UTF-8 -Dspring.cloud.consul.host=localhost -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:2345
 
 GOTO:MAIN
 
-@REM ############################# Common Functions ############################
+REM ############################# Common Functions ############################
 
-@REM Log trace.
-@REM %~1 Info message
+REM Log trace.
+REM %~1 Info message
 :logTrace
     ECHO %date% %time% TRACE - %~1%
 EXIT /B 0
 
-@REM Log info.
-@REM %~1 Info message
+REM Log info.
+REM %~1 Info message
 :logInfo
     ECHO %date% %time%  [32mINFO - %~1%[0m
 EXIT /B 0
 
-@REM Log warn.
-@REM %~1 Warn message
+REM Log warn.
+REM %~1 Warn message
 :logWarn
     ECHO %date% %time%  [33mWARN - %~1%[0m
 EXIT /B 0
 
-@REM Log error.
-@REM %~1 Error message
+REM Log error.
+REM %~1 Error message
 :logError
     ECHO %date% %time% [31mERROR - %~1%[0m
 EXIT /B 0
 
-@REM Set terminal title.
-@REM %~1 Title string
+REM Set terminal title.
+REM %~1 Title string
 :setTerminalTitle
     TITLE %~1
 EXIT /B 0
 
-@REM ############################ Custom Functions #############################
+REM ############################ Custom Functions #############################
 
-@REM Check Java major version
+REM Check Java major version
 :checkJavaMajorVersion
 CALL :logWarn "Start to Check Java major version…"
 %javaExe% -version 1>nul 2>nul || (
@@ -70,10 +70,10 @@ if %currentJavaMajorVersion% LSS %minimalJavaMajorVersion% (
   CALL :logError "Current Java version is too low, at least OpenJDK %minimalJavaMajorVersion% is needed"
   EXIT /B 1
 )
-CALL :logInfo "Passed Java major version cheking"
+CALL :logInfo "Passed Java major version checking"
 EXIT /B 0
 
-@REM Pull the latest code of current branch from Git.
+REM Pull the latest code of current branch from Git.
 :gitPull
     git pull
     if %ERRORLEVEL% NEQ 0 (
@@ -82,9 +82,9 @@ EXIT /B 0
     )
 EXIT /B 0
 
-@REM Execute pre-build phase.
+REM Execute pre-build phase.
 :executePreBuildPhase
-    CALL :logWarn "[PRE-BUILD] Active profile: %mavenActiveProfile%"
+    CALL :logWarn "[PRE-BUILD] Active profile: %springActiveProfile%"
     FOR /F "tokens=*" %%i IN ('git rev-parse --abbrev-ref HEAD') DO SET currentBranch=%%i
     CALL :logWarn "[PRE-BUILD] Current Git branch: %currentBranch%"
     if %skipGitPull% == true (
@@ -101,7 +101,7 @@ EXIT /B 0
     dir
 EXIT /B 0
 
-@REM Execute build phase.
+REM Execute build phase.
 :executeBuildPhase
     if %skipMavenBuild% == true (
         CALL :logWarn "[PRE-BUILD] Skipped Maven build"
@@ -109,7 +109,7 @@ EXIT /B 0
     )
     CALL :logInfo "[PRE-BUILD] Start to pull latest code from Git"
     SET currentDirectory=%cd%
-    @REM Change directory to root projeject
+    REM Change directory to root projeject
     cd ..
     CALL mvn clean package --batch-mode --show-version -Dmaven.javadoc.skip=true -DskipTests=true
     if %ERRORLEVEL% NEQ 0 (
@@ -121,45 +121,47 @@ EXIT /B 0
     dir
 EXIT /B 0
 
-@REM Execute run phase.
+REM Execute run phase.
 :executeRunPhase
-    CALL :logWarn "[RUN] Running service…"
-    FOR /F "tokens=*" %%i IN ('dir /B/A:- target\*.jar') DO SET jarFileName=%%i
+    FOR /F "tokens=* USEBACKQ" %%F IN (`dir /b /s /a:d "*-bootstrap"`) DO (SET bootstrapModulePath=%%F)
+    CALL :logWarn "[RUN] Found bootstrap module, about to run service. Bootstrap module path: %bootstrapModulePath%"
+    FOR /F "tokens=*" %%i IN ('dir /B/A:- %bootstrapModulePath%\target\*.jar') DO SET jarFileName=%%i
     if "%jarFileName%" == "" (
         CALL :logError "[RUN] Not found runnable JAR!"
         EXIT /B 1
     )
     CALL :logWarn "[RUN] Found JAR: %jarFileName%"
-    CALL :setTerminalTitle %jarFileName%
-    SET runJarCommand=%javaExe% %javaParameter% -Dspring.profiles.active=%mavenActiveProfile% -jar target\%jarFileName%
+    FOR %%a IN ("%~dp0\..") DO SET "baseProjectName=%%~nxa"
+    CALL :setTerminalTitle "%baseProjectName% :: %jarFileName%"
+    SET runJarCommand=%javaExe% %javaParameter% -Dspring.profiles.active=%springActiveProfile% -jar %bootstrapModulePath%\target\%jarFileName%
     CALL :logWarn "[RUN] Execute command: %runJarCommand%"
     CALL %runJarCommand%
 EXIT /B 0
 
-@REM ############################# MAIN Procedures #############################
+REM ############################# MAIN Procedures #############################
 :MAIN
 cls
 
-@REM Check Java major version
+REM Check Java major version
 CALL :checkJavaMajorVersion
 if %ERRORLEVEL% NEQ 0 (
     CALL :logError "Failed to check Java major version!"
     EXIT /B %ERRORLEVEL%
 )
 
-@REM Pre-build phrase (Display version, Git pull)
+REM Pre-build phrase (Display version, Git pull)
 CALL :executePreBuildPhase
 if %ERRORLEVEL% NEQ 0 (
     CALL :logError "[PRE-BUILD] Failed to execute pre-build phase"
     EXIT /B %ERRORLEVEL%
 )
-@REM Build phase
+REM Build phase
 CALL :executeBuildPhase
 if %ERRORLEVEL% NEQ 0 (
     CALL :logError "[BUILD] Failed to execute build phase"
     EXIT /B %ERRORLEVEL%
 )
-@REM Run phase
+REM Run phase
 CALL :executeRunPhase
 
 ENDLOCAL
