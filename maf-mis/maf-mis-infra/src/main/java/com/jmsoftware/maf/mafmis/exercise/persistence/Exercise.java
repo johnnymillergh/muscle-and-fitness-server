4a827f4eac11e847fa749fc2d08662f82fd1a5aa
Johnny Miller (ZA) <johnnysviva@outlook.com>
2022-02-03 21:00:14 +0800

    perf($maf-mis): DDD modularized microservice

---
+++ maf-mis/maf-mis-infra/src/main/java/com/jmsoftware/maf/mafmis/exercise/persistence/Exercise.java
@@ -0,0 +1,38 @@
+package com.jmsoftware.maf.mafmis.exercise.persistence;
+
+import com.jmsoftware.maf.common.bean.PaginationBase;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <h1>Exercise</h1>
+ * <p>
+ * https://exrx.net/Lists/Directory
+ *
+ * @author 钟俊 (jun.zhong), e-mail: jun.zhong@ucarinc.com
+ * @date 2/18/20 10:17 AM
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Exercise extends PaginationBase {
+    /**
+     * The ID of exercise.
+     */
+    private Long id;
+    /**
+     * The name of exercise.
+     */
+    private String name;
+    /**
+     * Exercise preparation description.
+     */
+    private String preparation;
+    /**
+     * Exercise execution description.
+     */
+    private String execution;
+    /**
+     * Exercise GIF image path.
+     */
+    private String exerciseGifPath;
+}
---
+++ maf-mis/maf-mis-message/src/main/java/com/jmsoftware/maf/mafmis/DelayedMessageListener.java
@@ -0,0 +1,26 @@
+package com.jmsoftware.maf.mafmis;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import static com.jmsoftware.maf.springcloudstarter.rabbitmq.DelayedMessageConfiguration.DELAYED_MESSAGE_QUEUE_NAME;
+
+/**
+ * <h1>DelayedMessageListener</h1>
+ * <p>
+ * Change description here.
+ *
+ * @author Johnny Miller (鍾俊), email: johnnysviva@outlook.com, 10/2/21 1:38 PM
+ **/
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DelayedMessageListener {
+    @RabbitListener(queues = DELAYED_MESSAGE_QUEUE_NAME)
+    public void receiveMessage(final Message message) {
+        log.info("Received message as a generic AMQP 'Message' wrapper: {}", message);
+    }
+}
