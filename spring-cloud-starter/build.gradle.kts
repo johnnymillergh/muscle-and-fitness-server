fba5361284ea75cd889492e8645bdbf7be9f92fb
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2022-08-01 08:01:57 +0800

    build($starter): compileOnly -> runtimeOnly

--- spring-cloud-starter/build.gradle.kts
+++ spring-cloud-starter/build.gradle.kts
@@ -63,9 +63,7 @@ dependencies {
     val mybatisPlusBootStarterVersion: String by project
     compileOnly("com.baomidou:mybatis-plus-boot-starter:$mybatisPlusBootStarterVersion")
     val shardingsphereVersion: String by project
-    compileOnly("org.apache.shardingsphere:shardingsphere-jdbc-core-spring-boot-starter:$shardingsphereVersion") {
-        exclude("com.squareup", "okhttp3")
-    }
+    runtimeOnly("org.apache.shardingsphere:shardingsphere-jdbc-core-spring-boot-starter:$shardingsphereVersion")
 
     // POI
     val poiVersion: String by project
